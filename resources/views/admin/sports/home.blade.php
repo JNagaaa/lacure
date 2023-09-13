41a239608ba0d27637abb76a2e36b7aecd8c2291
jordan <jordan.grauwmans@eezee-it.com>
2023-09-13 21:57:40 +0200

    Fix frontend admin sports

--- resources/views/admin/sports/home.blade.php
+++ resources/views/admin/sports/home.blade.php
@@ -1,27 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
+<div class="container-fluid" style="background-color: #333333; color: #FFFFFF; padding: 20px; border-radius: 10px;">
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <div>
-
-
+            <div style="border: 1px solid #FFA500; padding: 25px; border-radius: 10px; background-color: #555555;">
                 <div class="text-center">
-                    <h1>Gestion portail Horeca</h1>
+                    <h1>Gestion portail Sports</h1>
                     <div class="row">
                         <div class="col-md-6 d-flex justify-content-center">
-                            <a href='/admin/sports/fields/list' class="btn btn-primary btnAdmin btnFields btn-lg m-3 rounded-pill">
+                            <a href='/admin/sports/fields/list' class="btn btn-primary btnAdmin btnFields btn-lg m-3 rounded-pill" style="background-color: #FFA500; border-color: #FFA500;">
                                 {{ __('Terrains')}}
                             </a>
                         </div>
                         <div class="col-md-6 d-flex justify-content-center">
-                            <a href='/admin/sports/timeslots/list' class="btn btn-primary btnAdmin btnTimeslots btn-lg m-3 rounded-pill">
+                            <a href='/admin/sports/timeslots/list' class="btn btn-primary btnAdmin btnTimeslots btn-lg m-3 rounded-pill" style="background-color: #FFA500; border-color: #FFA500;">
                                 {{ __('Plages horaires')}}
                             </a>
                         </div>
                         <div class="col-md-12 d-flex justify-content-center">
-                            <a href='/admin/sports/news/list' class="btn btn-primary btnAdmin btnNews btn-lg m-3 rounded-pill">
+                            <a href='/admin/sports/news/list' class="btn btn-primary btnAdmin btnNews btn-lg m-3 rounded-pill" style="background-color: #FFA500; border-color: #FFA500;">
                                 {{ __('Actualités')}}
                             </a>
                         </div>
